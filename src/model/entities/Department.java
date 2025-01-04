5c34da10019d97e0a0fe2c9ec4c86e0792d6b6e4
Jose Vitor <josevitorbarroso123@gmail.com>
2025-01-04 15:41:44 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,56 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private Integer id;
+    private String nome;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department {" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+
+}
